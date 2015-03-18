cd0328ef413a0a0cced261a83b071a98a1379ebc
David Villa Alises <David.Villa@gmail.com>
2015-03-18 16:39:35 +0100

    run only a test

--- bash/unittest.sh
+++ bash/unittest.sh
@@ -9,14 +9,26 @@ function testfail {
 	sc-log-fail $testname
 }
 
+function run-test {
+	local testname=$1
+    (
+    	sc-set-trap testfail
+    	eval $testname
+    	sc-log-ok $testname
+    	sc-clear-trap
+    )
+}
+
 function run-testsuit {
+	local testname=$1
+
+	if [[ -n $testname ]]; then
+		run-test $testname
+		return
+	fi
+
     grep "^function test-" $__file__ | while read line; do
         testname=$(echo $line | cut -d' ' -f 2)
-    	(
-    	    sc-set-trap testfail
-    	    eval $testname
-    	    sc-log-ok $testname
-    	    sc-clear-trap
-    	)
+		run-test $testname
     done
 }
