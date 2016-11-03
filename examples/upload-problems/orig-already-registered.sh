e64009cbba69d38f7b1d8669d07e1674217aecce
David Villa Alises <David.Villa@gmail.com>
2016-11-03 18:52:10 +0000

    reset example

--- examples/upload-problems/orig-already-registered.sh
+++ examples/upload-problems/orig-already-registered.sh
@@ -1,7 +1,9 @@
 #!/bin/bash --
 # -*- coding: utf-8; mode: shell-script; tab-width: 4 -*-
 
+hg revert debian/changelog
 echo 1 > content
+ian release -qi
 ian build -c
 ian upload
 echo 2 > content
