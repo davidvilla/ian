fed27d4d137a23c6612757014c3492172a1a34bb
David Villa Alises <David.Villa@gmail.com>
2016-11-07 11:52:59 +0000

    fixed "cmd:release" and _micro-version

--- ian-hooks.sh
+++ ian-hooks.sh
@@ -4,46 +4,46 @@
 function notify-clean {
     if sc-function-exists ian-clean-hook; then
 		log-info "exec ian-clean-hook"
-		ian-run ian-clean-hook &
+		ian-run ian-clean-hook
     fi
 }
 
 function notify-release {
     if sc-function-exists ian-release-hook; then
 		log-info "exec ian-release-hook"
-		ian-run ian-release-hook &
+		ian-run ian-release-hook
     fi
 }
 
 function notify-build-start {
     if sc-function-exists ian-build-start-hook; then
 		log-info "exec ian-build-start-hook"
-		ian-run ian-build-start-hook &
+		ian-run ian-build-start-hook
     fi
 }
 
 function notify-build-end {
     if sc-function-exists ian-build-end-hook; then
 		log-info "exec ian-build-end-hook"
-		ian-run ian-build-end-hook &
+		ian-run ian-build-end-hook
     fi
 
     if sc-function-exists ian-build-hook; then
 		log-info "exec ian-build-end-hook"
-		ian-run ian-build-hook &
+		ian-run ian-build-hook
     fi
 }
 
 function notify-install {
     if sc-function-exists ian-install-hook; then
 		log-info "exec ian-install-hook"
-		ian-run ian-install-hook &
+		ian-run ian-install-hook
     fi
 }
 
 function notify-upload {
     if sc-function-exists ian-upload-hook; then
 		log-info "exec ian-upload-hook"
-		ian-run ian-upload-hook &
+		ian-run ian-upload-hook
     fi
 }
