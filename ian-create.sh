42d205ae378e6338050d09e3e2e29603b371778d
David Villa Alises <david.villa@gmail.com>
2025-11-26 18:51:28 +0100

    update standars version

--- ian-create.sh
+++ ian-create.sh
@@ -60,7 +60,7 @@ Section: utils
 Priority: optional
 Maintainer: $DEBFULLNAME <$DEBEMAIL>
 Build-Depends: debhelper (>= 7.0.50~), debhelper-compat (= 12), quilt
-Standards-Version: 4.6.2
+Standards-Version: 4.7.2
 
 Package: $pkgname
 Architecture: all
